bc2bd35d385cdede75419d0989782db2fd7f1799
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-09-16 16:12:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+var name = `
 
+█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█
+█░░║║║╠─║─║─║║║║║╠─░░█
+█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█
+█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+
+ls to know more :)  
+`
+function changeToHackerTheme() {
+    // Select the elements you want to change colors for (e.g., all elements with text color)
+    const elementsToChange = document.querySelectorAll('*');
+  
+    // Iterate through the selected elements and change their color to green
+    elementsToChange.forEach((element) => {
+      element.style.color = 'green';
+    });
+  }
 function font(){
     var large_name = `
     ##     ## ####  ######  ##     ## ##    ## ##     ##     ########  ########  #### ##    ##    ###    ##    ## 
@@ -24,11 +42,85 @@ function font(){
         }
     });
 }
+var profile = true;
+var password = true;
 $(document).ready(function () {
-    $('#terminal').terminal(function (command, term ) {
+    $('#terminal').terminal(function (command , term ) {
+        const history = term.history().data()
+        grep = history[history.length -2]
+        grep2 = history[history.length -3]
+        function validate(){
+            if(grep == 'cd password' | grep == 'cat secret'){
+                window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUOcmljayByb2xsIHNvbmc%3D'
+    }
+    else{
+        term.echo('not valid')
+    }
+}
+
+function which_ls(){
+    if(grep == 'cd password'){
+        term.echo('secret')
+    }
+    else if (grep == 'cd profile'){
+        term.echo(`[[;#0ff;]summary ] \t`,{newline: false})
+        term.echo(`[[;#0ff;]about] \t`,{newline: false})
+        term.echo(`[[;#0ff;]blog] \t`,{newline: false})
+        term.echo(`[[;#0ff;]other] \t`)
+ 
+    }
+    else{
+         //term.echo(`[[;#0ff;]Here is list of available commands ]`)
+            //term.echo(`Use cd to navigate to directory`)
+            term.echo(`[[;white;]profile/] \t`, { newline: false })
+            term.echo(`[[;#0ff;]pwd] \t`,{newline: false})
+            term.echo(`[[;white;]password/]`)
+                 
+    }
+}
+
         switch (command) {
             case 'ls':
-                term.echo(`about    certificates    summary     blog `)
+                which_ls()
+                break;
+
+             
+            case 'cd password':
+                term.set_prompt('[[;white;]root@vishnu/password >> ]')
+                break;
+            
+            case 'cd profile':
+                term.set_prompt('[[;white;]root@vishnu/profile >> ]')
+                break;
+
+            case 'cat secret':
+                if(profile == true){}
+                validate()
+                break;
+                case 'cd home':
+                    term.set_prompt('[[;white;]root@vishnu >> ]')
+                    break;
+
+                case 'hi':
+                    term.echo('bro')
+                    break;
+            case 'cat summary':
+                term.echo(`
+    name: 
+    [[;#0ff;]Vishnupriyan]`);
+    term.echo(`
+    email: 
+    vishnupriyan1904@gmail.com`);
+    term.echo(`
+    github:
+    https://github.com/vishnu7priyan`)
+    term.echo(`
+    twitter: 
+    https://twitter.com/vishnuwebsec`);
+    term.echo(`
+    blog: 
+    https://cyberdug.hashnode.dev/`);
+
                 break;
 
             case 'pwd':
@@ -54,14 +146,16 @@ $(document).ready(function () {
 
                 term.echo(resumeLink)
                 break;
-            case 'about':
+            case 'cat about':
                 // link element
                 const aboutLink = document.createElement('a');
-                aboutLink.href = '/about.html'; 
+                aboutLink.href = '/about/about.html'; 
                 aboutLink.textContent = 'click here to read more about me';
 
+                // css styles to change the link color
                 aboutLink.style.color = 'pink'; 
 
+                // append the link element to the terminal
                 term.echo('');
                 term.echo(aboutLink);
 
@@ -76,16 +170,10 @@ $(document).ready(function () {
                 break; 
 
             case 'clear':
-                    term.echo('command no')
+                    term.echo('command no') //this doesnt work ????? try something else
                 break;
 
-            case 'test':
-                term.exec([
-                    'pwd',
-                    'cat blog',
-                    'cat resume'
-                 ]);
-                break;   
+           
                 
             case 'cd blog ':
             case 'cd about':
@@ -104,14 +192,20 @@ $(document).ready(function () {
                //term.setOptions({ fontSize: 14 });
                terminal.disable;
                break;
-            default:
-                term.echo(" ");
+            case 'summary':
+            case 'blog':
+            case 'about':
+            case 'resume':
+                if (profile == true){
+                    term.echo('use cat to view')
+                }
                 break;
+            
         }
     }, {
         greetings: vishnu.innerHTML,
 
-        prompt: '[[;lightgreen;]>>> ] ',
+        prompt: '[[;white;]root@vishnu >> ] ',
         onInit: function (term) {
             // Enable HTML rendering for greetings
             term.echo('', {
@@ -120,3 +214,6 @@ $(document).ready(function () {
         }
     });
 });
+
+
+
