22ec935dab728c1324f98466341a1d529a28d1ea
Vishnu7priyan <96537513+Vishnu7priyan@users.noreply.github.com>
2023-09-22 21:56:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,3 +42,49 @@ function font(){
         }
     });
 }
+
+var profile = false;
+var password = false;
+var home = true;
+$(document).ready(function () {
+    $('#terminal').terminal(function (command , term ) {
+        switch (command) {
+            case 'ls':
+                if(home == true){
+                    term.echo(`[[;white;]profile/] \t`, { newline: false })
+                    term.echo(`[[;#0ff;]pwd] \t`,{newline: false})
+                    term.echo(`[[;white;]password/] \t`,{newline: false})
+                    term.echo(`[[;#0ff;]banner] \t`,{newline: false})
+                    term.echo(`[[;#0ff;]history]`)
+                }
+                else if(password==true){
+                    term.echo('secret')
+                }
+                else if (profile == true){
+                    term.echo(`[[;#0ff;]summary ] \t`,{newline: false})
+                    term.echo(`[[;#0ff;]about] \t`,{newline: false})
+                    term.echo(`[[;#0ff;]blog] \t`,{newline: false})
+                    term.echo(`[[;#0ff;]other] \t`)
+                }
+                break
+            
+            case 'cd ..':
+                case 'cd':
+                if (home == true){
+                    term.echo('you are already in home :)')
+                }
+                else{
+                term.set_prompt('[[;white;]vishnu@terminal >> ]')
+                home = true;
+                password=false;
+                profile=false;}
+                break;    
+
+
+            case 'cd password':
+                if (home == true){
+                    term.set_prompt('[[;white;]vishnu@terminal ~/password >> ]')
+                password = true;
+                home = false;
+                profile = false;
+                }
